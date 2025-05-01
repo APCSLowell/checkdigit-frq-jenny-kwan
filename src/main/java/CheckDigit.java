2261a273c8f7214174a42eeb65a59fb2544b8e6a
jenny-kwan <jennykwan222@gmail.com>
2025-05-01 13:20:02 -0700

    Update CheckDigit.java

--- src/main/java/CheckDigit.java
+++ src/main/java/CheckDigit.java
@@ -1,31 +1,32 @@
 public class CheckDigit 
 {   
-  /** Returns the check digit for num.
-   *  Precondition: num has 1 to 6 digits, num >= 0
-   */
+  /** Returns the check digit for num, as described in part (a).  
+   *  Precondition: The number of digits in num is between one and    
+   *  six, inclusive.  
+   *          num >= 0  
+   */  
   public static int getCheck(int num) 
   {  
     int sum = 0;
-    int numDigits = getNumberOfDigits(num);
-
-    for (int i = 1; i <= numDigits; i++) 
+    int n = getNumberOfDigits(num);
+    for (int i = 1; i <= n; i++)
     {
       sum += getDigit(num, i) * i;
     }
-
     return sum % 10;
   }
 
-  /** Returns true if numWithCheckDigit is valid, or false otherwise.
-   *  Precondition: numWithCheckDigit has 2 to 7 digits, num >= 0
-   */
+  /** Returns true if numWithCheckDigit is valid, or false    
+   *  otherwise, as described in part (b). 
+   *  Precondition: The number of digits in numWithCheckDigit   
+   *  is between two and seven, inclusive.
+   *                numWithCheckDigit >= 0     
+   */     
   public static boolean isValid(int numWithCheckDigit)    
   {      
-    int numDigits = getNumberOfDigits(numWithCheckDigit);
-    int num = numWithCheckDigit / 10; // original number
-    int checkDigit = numWithCheckDigit % 10; // last digit
-
-    return getCheck(num) == checkDigit;
+    int original = numWithCheckDigit / 10;
+    int lastDigit = numWithCheckDigit % 10;
+    return getCheck(original) == lastDigit;
   }    
    
   /** Returns the number of digits in num. */    
@@ -33,12 +34,11 @@ public class CheckDigit
   {      
     if(num < 10)
       return 1;
-    return 1 + getNumberOfDigits(num / 10);    
+    return 1 + getNumberOfDigits(num/10);    
   }    
    
-  /** Returns the nth digit of num.
-   *  Precondition: n >= 1 and n <= number of digits
-   *  Example: getDigit(12345, 1) returns 1; getDigit(12345, 5) returns 5
+  /** Returns the nth digit of num.      
+   *  Precondition: n >= 1 and n <= the number of digits in num     
    */    
   public static int getDigit(int num, int n)    
   {      
