7dfc8e026642c7f9963006b8d7d62b9fc7d9699d
jenny-kwan <jennykwan222@gmail.com>
2025-05-01 13:26:35 -0700

    Update CheckDigit.java

--- src/main/java/CheckDigit.java
+++ src/main/java/CheckDigit.java
@@ -1,40 +1,34 @@
-public class CheckDigit 
-{   
-  public static int getCheck(int num) 
-  {  
-    int sum = 0;
-    int numDigits = getNumberOfDigits(num);
+public class CheckDigit {
 
-    for (int i = 1; i <= numDigits; i++) 
-    {
-      int digit = getDigit(num, i); // i=1 is leftmost
-      int weight = numDigits - i + 1; // position from right
-      sum += digit * weight;
+    public static int getCheck(int num) {
+        int sum = 0;
+        int numDigits = getNumberOfDigits(num);
+
+        for (int i = 1; i <= numDigits; i++) {
+            int digit = getDigit(num, i);
+            sum += digit * i;  
+        }
+
+        return sum % 10;
     }
 
-    return sum % 10;
-  }
-
-  public static boolean isValid(int numWithCheckDigit)    
-  {      
-    int checkDigit = numWithCheckDigit % 10;
-    int original = numWithCheckDigit / 10;
-    return getCheck(original) == checkDigit;
-  }
-
-  public static int getNumberOfDigits(int num)    
-  {      
-    if (num < 10) return 1;
-    return 1 + getNumberOfDigits(num / 10);    
-  }
-
-  public static int getDigit(int num, int n)    
-  {      
-    int pos = getNumberOfDigits(num) - n + 1;
-    while (pos > 1) {
-      num /= 10;
-      pos--;
+    public static boolean isValid(int numWithCheckDigit) {
+        int checkDigit = numWithCheckDigit % 10;
+        int original = numWithCheckDigit / 10;
+        return getCheck(original) == checkDigit; 
     }
-    return num % 10;
-  }     
+
+    public static int getNumberOfDigits(int num) {
+        if (num < 10) return 1;
+        return 1 + getNumberOfDigits(num / 10);
+    }
+
+    public static int getDigit(int num, int n) {
+        int pos = getNumberOfDigits(num) - n + 1; 
+        while (pos > 1) {
+            num /= 10;
+            pos--;
+        }
+        return num % 10;  
+    }
 }
